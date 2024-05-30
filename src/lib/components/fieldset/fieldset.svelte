ade93abcc3008c607ae3d8231ee7f989bd142d3c
Forthtilliath <forth@live.fr>
2024-05-30 17:35:01 +0200

    style: change fieldset border style

--- src/lib/components/fieldset/fieldset.svelte
+++ src/lib/components/fieldset/fieldset.svelte
@@ -23,19 +23,36 @@
 </fieldset>
 
 <style lang="scss">
+	$color: #0071f0;
+	$background:
+		linear-gradient(#020817, #020817) padding-box,
+		linear-gradient(to right, #38bdf8, #bae6fd, #38bdf8) border-box;
+
 	fieldset {
-		--color: #0071F0;
-		border: 2px solid var(--color);
+		// border: 2px solid $color;
+		// border-image: linear-gradient(#38bdf8, #bae6fd, #38bdf8) 1;
+		background: $background;
+		border: 2px solid transparent;
 		border-radius: 5px;
 		margin-bottom: 20px;
 		padding: 10px 20px;
 
 		legend {
-			background-color: var(--color);
+			// background-color: $color;
+			background: $background;
+			border: 2px solid transparent;
 			color: white;
-			padding: 5px 10px;
 			border-radius: 5px;
 			font-weight: bold;
+
+			&:has(button) {
+				button {
+					padding: 5px 10px;
+				}
+			}
+			&:not(:has(button)) {
+				padding: 5px 10px;
+			}
 		}
 	}
 </style>
