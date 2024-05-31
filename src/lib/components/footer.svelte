e7d92a6d246dedacda101980a3ac1f87580b3629
Forthtilliath <forth@live.fr>
2024-05-31 22:08:31 +0200

    chore: add footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Logo from '$lib/assets/logo.svelte';
+	import Logo2 from '$lib/assets/logo.jpg';
+</script>
+
+<footer class="container px-5 py-5 mx-auto flex items-center md:flex-row flex-col justify-center">
+	<a
+		class="flex title-font font-medium items-center md:justify-start justify-center text-slate-200"
+		href="/"
+	>
+		
+    <!-- <Logo class="w-12 h-12 sm:w-20 sm:h-20" /> -->
+    <img src={Logo2} class="w-8 h-8 sm:w-10 sm:h-10 rounded-lg" alt="Logo" />
+
+		<span class="ml-3 text-xl">LOL Nuclear Random Arena</span>
+	</a>
+	<p
+		class="text-sm text-slate-500 md:ml-4 md:pl-4 md:border-l-2 md:border-gray-200 md:py-2 md:mt-0 mt-4"
+	>
+		© 2024 LOL Nuclear Random Arena —
+		<a
+			href="https://github.com/Forthtilliath"
+			class="text-gray-600 ml-1"
+			rel="noopener noreferrer"
+			target="_blank">@forth</a
+		>
+	</p>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,23 @@
 <script>
+	import Footer from '$lib/components/footer.svelte';
 	import '../app.css';
-  import { Toaster } from 'svelte-sonner'
+	import { Toaster } from 'svelte-sonner';
 
 	// TODO Add loader
 </script>
 
 <svelte:head>
 	<title>LoL Random Arena</title>
-	<meta name="description" content="Random Team & Champion Generator for League of Legends Arena Matches">
+	<meta
+		name="description"
+		content="Random Team & Champion Generator for League of Legends Arena Matches"
+	/>
 </svelte:head>
 
 <Toaster />
-<slot />
-<footer class="mt-4"></footer>
+<section class="min-h-[100dvh] flex flex-col justify-between">
+	<main>
+		<slot />
+	</main>
+	<Footer />
+</section>
